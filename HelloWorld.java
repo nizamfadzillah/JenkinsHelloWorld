eba380067dd7cf5e7a5c86325510714a5c9a987b
nizamfadzillah <85063205+nizamfadzillah@users.noreply.github.com>
2024-10-04 00:56:22 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Make some changes");
+        System.out.println("Why Not");
     }
 }
